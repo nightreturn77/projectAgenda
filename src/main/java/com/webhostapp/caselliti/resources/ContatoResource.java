d3b3c4e9ce5505fc34a524acffbfa8f5c8eb665c
Victor Dias <victordcporto@gmail.com>
2020-01-15 01:45:34 -0300

    delete e findById

--- src/main/java/com/webhostapp/caselliti/resources/ContatoResource.java
+++ src/main/java/com/webhostapp/caselliti/resources/ContatoResource.java
@@ -6,11 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 
 import com.webhostapp.caselliti.entity.Contato;
 import com.webhostapp.caselliti.services.ContatoService;
@@ -47,6 +48,21 @@ public class ContatoResource {
 		return ResponseEntity.created(uri).body(obj);
 		
 	}
+	
+	@GetMapping(value = "/json/{id}")
+	public ResponseEntity<Contato> procurarId(@PathVariable Long id){ 
+		Contato cont = service.findById(id);
+		return ResponseEntity.ok().body(cont);
+	}
+	
+	@DeleteMapping(value = "/json/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){ 
+		service.delete(id);
+		return ResponseEntity.ok().build();
+	}
+	
+	
+	
 	
 	
 	
